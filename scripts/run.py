035b7438885d784fe67d2ed2ed915eed51a4d1e8
Ivan <ivanalsina@hotmail.com>
2022-02-02 18:20:42 +0100

    Add script run.py

---
+++ scripts/run.py
@@ -0,0 +1,17 @@
+import json
+from wiktionaryparser import BaseParser, EnglishParser, SpanishParser
+from deepdiff import DeepDiff
+from typing import Dict, List
+import mock
+from urllib import parse
+import os
+from wiktionaryparser.logger import autolog
+
+english_parser = EnglishParser()
+spanish_parser = SpanishParser()
+
+autolog('PARSING "HOUSE"', 3)
+english_output = english_parser.fetch('house')
+
+autolog('PARSING "CASA"', 3)
+spanish_output = spanish_parser.fetch('casa')
