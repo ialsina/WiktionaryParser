06e9a0fd2f7a3d3afddd3d84ff1cd91c63bc0019
Ivan Alsina <ivanalsina@protonmail.com>
2022-08-21 14:43:03 +0200

    Code cleanup

--- scripts/tests.py
+++ scripts/tests.py
@@ -13,14 +13,6 @@ targets = [
     'foamy'
 ]
 
-# user_inp = input('Input word to search:\n>')
-# user_inp = 'table'
-
-# retrieved = parser.fetch(user_inp, 'english', wordclass=False)
-# word_contents = parser.get_word_contents('english')
-# tr = parser.parse_translations(wc, 0)
-# wdo = parser.map_to_object(retrieved)
-
 json_contents = {}
 words = {}
 translations_russian = {}
